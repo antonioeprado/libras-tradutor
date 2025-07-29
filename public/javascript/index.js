f6de08b66a66ad78217cc1e61a20310fd2bb2837
Antonio Prado <antonioeprado18@gmail.com>
2025-07-29 08:00:35 -0300

    first commit

---
+++ public/javascript/index.js
@@ -0,0 +1,31 @@
+const submitButton = document.getElementById("submit")
+// const files = document.getElementById("txt-input").files
+const preview = document.getElementById("preview")
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    handleFiles()
+})
+
+function handleFiles() {
+
+    const files = document.getElementById("txt-input").files
+    if (!files[0].type.startsWith("text")) {
+        console.error("Wrong filetype!!")
+        window.alert("Tipo errado de arquivo!")
+        return;
+    }
+    const text = document.createElement("p")
+    text.classList.add("obj")
+    text.file = files[0]
+    preview.appendChild(text)
+
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        text.innerText = e.target.result
+    }
+    reader.readAsText(files[0])
+
+}
+
